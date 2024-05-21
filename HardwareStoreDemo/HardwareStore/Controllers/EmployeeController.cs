8c317ac4073be47aafda94b5e00bee2aebf9482a
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-21 10:15:22 -0600

    lol

---
+++ HardwareStoreDemo/HardwareStore/Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using HardwareStore.Models;
+using HardwareStore.Repositories.Employees;
+using HardwareStore.Validations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HardwareStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IValidator<Employee> _validator;
+
+        public EmployeeController(IEmployeeRepository employeeRepository, IValidator<Employee> validator)
+        {
+            _employeeRepository = employeeRepository;
+            _validator = validator;
+        }
+
+        // GET: EmployeeController
+        public async Task<ActionResult> Index()
+        {
+            var employees = await _employeeRepository.GetAllAsync();
+
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Employee employee)
+        {
+            try
+            {
+                ValidationResult validationResult =
+                    await _validator.ValidateAsync(employee);
+
+                if (!validationResult.IsValid)
+                {
+                    validationResult.AddToModelState(this.ModelState);
+
+                    return View(employee);
+                }
+
+                await _employeeRepository.AddAsync(employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+
+                return View(employee);
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Employee employee)
+        {
+            try
+            {
+                await _employeeRepository.EditAsync(employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(employee);
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(Employee employee)
+        {
+            try
+            {
+                await _employeeRepository.DeleteAsync(employee.Id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(employee);
+            }
+        }
+    }
+}
---
+++ HardwareStoreDemo/HardwareStore/Program.cs
@@ -0,0 +1,40 @@
+using FluentValidation;
+using HardwareStore.Data;
+using HardwareStore.Models;
+using HardwareStore.Repositories.Employees;
+using HardwareStore.Validations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IValidator<Employee>, EmployeeValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
